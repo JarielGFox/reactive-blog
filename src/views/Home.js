f7b4de56ca9b44d2c0e5f6e87a767d140c8327bf
Giovanni Crudele <crudelegio@gmail.com>
2023-07-27 19:16:10 +0200

    home component button

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Ciao');
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
