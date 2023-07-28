79b60be511420f16d20a1d60f41dabb05590e859
Giovanni Crudele <crudelegio@gmail.com>
2023-07-28 12:24:27 +0200

    use of map to make a list of elements

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,21 @@
-const Home = () => {
+import { useState } from "react"; // prima destrutturiamo l'hook dello useState e poi importiamo dalla libreria di React;
 
-    const handleClick = () => {
-        console.log('Ciao');
-    }
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((element) => (
+                <div className="blog-preview" key={element.id}>
+                    <h2>{element.title}</h2>
+                    <p>Written by {element.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
