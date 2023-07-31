c18e81ab98e11fe8882b2cabed616d163a65ac11
JarielGFox <crudelegio@gmail.com>
2023-07-31 17:04:33 +0200

    error managing and ternary operator to manage conditions

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,29 +2,43 @@ import { useState, useEffect } from "react"; // prima destrutturiamo l'hook dell
 import BlogList from "../components/BlogList";
 
 const Home = () => {
-    // blogs è un array di oggetti al quale applichiamo il metodo map()
+    // vari useState che ci permettono di interagire
+
+    // questo prende i dati dal server 
     const [blogs, setBlogs] = useState(null);
+    // questo permette che si visualizzi il loading message
     const [isPending, setIsPending] = useState(true);
+    // questo per gestire i messaggi di errore
+    const [error, setError] = useState(null);
 
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
+            .then(response => {
+                if (!response.ok) {
+                    throw Error('Impossible to fetch data');
+                }
+                return response.json()
             })
             .then(data => {
                 console.log(data);
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
             })
+            .catch((err => {
+                setIsPending(false);
+                setError(err.message);
+            }))
     }, [])
 
     return (
         <div className="home">
-            {/* il "logical and" fa si che se la condizione a sx è vera, ciò che è a dx viene mostrato nel browser */}
-            {isPending && <div>Loading...</div>}
+            {error ? <div>{error}</div> : null}
+            {/*ternary operator fa si che se la condizione a sx è vera, ciò che è a dx viene mostrato nel browser, altrimenti mostra niente*/}
+            {isPending ? <div>Loading...</div> : null}
             {/* blogPosts è una prop così come title */}
-            {blogs && <BlogList blogPosts={blogs} title="Blogs List" />}
+            {blogs ? <BlogList blogPosts={blogs} title="Articles List" /> : null}
         </div>
     );
 
