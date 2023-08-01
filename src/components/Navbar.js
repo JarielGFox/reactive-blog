a2033c11d10f74360d7509ce97273a5fb03987bd
JarielGFox <crudelegio@gmail.com>
2023-08-01 16:09:01 +0200

    usage of Link routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
+// importiamo Link dalla libreria
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>React-ive Blog</h1>
 
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                {/* nei link tags c'è una funz speciale che previene la richiesta al server */}
+                {/* tramite il router, inietta nel browser il percorso al singolo componente */}
+                <Link to="/">Home</Link>
+                <Link to="/create">New Article</Link>
             </div>
         </nav>
     );
