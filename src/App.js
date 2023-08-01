0a0bf9c5c9b0e295cf7b6870052c412200122714
JarielGFox <crudelegio@gmail.com>
2023-08-01 13:27:49 +0200

    successfully linked a new component to routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+// per il routing importiamo queste proprietà
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./views/Home";
+import Create from "./components/Create";
 
 function App() {
   return (
+    // utilizziamo il componente Router per far capire che stiamo facendo il Routing
     <Router>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <div className="content">
+          <Routes>
+            {/* qui dentro ci sono i percorsi ai singoli componenti */}
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />}></Route>
+          </Routes>
+        </div>
       </div>
     </Router>
   );
---
+++ src/components/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return (
+        <div className="create">
+            <h2>Add a new article</h2>
+        </div>
+    );
+}
+
+export default Create;
