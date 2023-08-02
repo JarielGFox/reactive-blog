b0976795cdcdd2b6c2e9fdb18d7b1c3ec2a0f586
JarielGFox <crudelegio@gmail.com>
2023-08-02 16:53:07 +0200

    delete articles through delete method

--- src/views/ArticleDetails.js
+++ src/views/ArticleDetails.js
@@ -1,11 +1,31 @@
 // ci permette di prendere dei parametri dalle rotte
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import useFetch from "../useFetch";
+import { useState } from "react";
 
 const ArticleDetails = () => {
     // passiamo i parametri corretti da usare
     const { id } = useParams();
     const { data: article, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const navigate = useNavigate();
+    const [deleteError, setDeleteError] = useState();
+
+
+    const handleClick = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/blogs/' + article.id, {
+                method: 'DELETE'
+            });
+
+            if (!response.ok) {
+                throw new Error('Could not delete the article');
+            }
+
+            navigate('/');
+        } catch (error) {
+            setDeleteError(error.message);
+        }
+    }
 
     return (
         <div className="article-details">
@@ -16,6 +36,8 @@ const ArticleDetails = () => {
                     <h2>{article.title}</h2>
                     <p>Written by {article.author}</p>
                     <div>{article.body}</div>
+                    <button onClick={handleClick}>Delete</button>
+                    {deleteError ? <div>{deleteError}</div> : null}
                 </article>
             ) : null}
         </div>
